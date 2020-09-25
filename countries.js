ee67ae9a7f087919182ddf77f2c6464b1ccfca01
JJ Asghar <jjasghar@gmail.com>
2020-09-25 16:34:29 -0500

    added first pass

--- countries.js
+++ countries.js
@@ -1,7 +1,19 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
-    ITA: {fillKey: 'visited'},
     USA: {fillKey: 'visited'},
-    THA: {fillKey: 'visited'},
+    CAN: {fillKey: 'visited'},
+    CHE: {fillKey: 'visited'},
+    CRI: {fillKey: 'visited'},
+    DEU: {fillKey: 'visited'},
+    BEL: {fillKey: 'visited'},
+    MEX: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+    SWE: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    JPN: {fillKey: 'visited'},
+    JPN: {fillKey: 'visited'},
+    CHN: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
 }
